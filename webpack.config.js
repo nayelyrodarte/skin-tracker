4ae007a0f132e38feaa38439e1fae3b715eb5f32
Nayely Rodarte <nayelyrodarte@gmail.com>
2020-10-14 17:18:15 -0500

    Update package.json & add webpack.config file

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const miniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './scripts.js',
+  output: {
+    path: __dirname + '/build',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new miniCSSExtractPlugin({
+      filename: 'bundle.css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+          { loader: miniCSSExtractPlugin.loader },
+        ],
+      },
+    ],
+  },
+};
