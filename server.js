3a9ce487740a04a9ac7401ffc53d57aa08df980c
Nayely Rodarte <nayely@coolhumans.team>
2021-06-07 12:40:13 -0500

    Add config to stop serving api paths as static

--- server.js
+++ server.js
@@ -24,7 +24,9 @@ app.listen(PORT, host, function (err) {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "build")));
+  app.get(/^((?!(api)).)*$/, (req, res) =>
+    res.sendFile(path.resolve(__dirname, "build"))
+  );
 }
 
 mongoose
