158cc01ce6cd374ef1dcd02759370874cc633d72
Nayely Rodarte <nayely@coolhumans.team>
2021-06-07 10:33:16 -0500

    Change console.log copy on app.listen

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv').config();
-const mongoose = require('mongoose');
-const cors = require('cors');
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 // middleware
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
-app.use('/api/routine', require('./API/routes/routine'));
-app.use('/', express.static('build'));
+app.use("/api/routine", require("./API/routes/routine"));
+app.use("/", express.static("build"));
 
 const PORT = process.env.PORT || 3000;
-const host = '0.0.0.0' || 'localhost';
+const host = "0.0.0.0" || "localhost";
 
 app.listen(PORT, host, function () {
-  console.log('Levantando servidor en');
-  console.log(`http://localhost:${process.env.PORT}`);
+  console.log(`Levantando servidor en ${process.env.PORT}`);
 });
 
 mongoose
@@ -30,7 +29,7 @@ mongoose
     useFindAndModify: false,
   })
   .then(function () {
-    console.log('connected with Mongo');
+    console.log("connected with Mongo");
   })
   .catch(function (err) {
     console.log(err);
